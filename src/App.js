bf26946d8d3679a5d2d264cbf18ed6945e391ab0
Gerardo David Moratinos Meza <gmoratinos8@outlook.es>
2023-03-19 19:10:50 -0700

    First steps made, inputs and tags

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { FiEye } from 'react-icons/fi';
+
 import './App.css';
 
 function App() {
+ 
+  
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePasswordVisibility = () => setShowPassword(!showPassword);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Login__Container">
+        <form action="#" method="POST">
+        <label htmlFor="Username">Username:</label> <br/>
+        <input id="Username" type="text" placeholder="Username" required minLength={6} maxLength={16} title="El usuario debe tener al menos 6 Caracteres"></input><br/>
+        <label htmlFor="Password"> Password:</label><br/>
+        <input type={showPassword ? 'text' : 'password'} id="Password" placeholder="Password" required title="This field is required, the password should has Numbers letters and special characters"minLength={6} maxLength={16}/>
+        <button id="showpassword" type="button" onClick={togglePasswordVisibility}><FiEye/></button><br/>
+        <span>The password should have at least 8 characters </span>
+
+        <div className="button__container">
+        <button id="login__button" type="submit"> Log in</button>
+        <button id="sign__in__button" type="submit">Sign in</button>
+        </div>
+        </form>
+        </div>
       </header>
     </div>
   );
