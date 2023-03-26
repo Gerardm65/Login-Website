57b5754aed1ea4cafdb05de707addb468979bb7b
Gerardo David Moratinos Meza <gmoratinos8@outlook.es>
2023-03-25 22:50:01 -0700

    Added animation to log in button

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ function App() {
     const togglePasswordVisibility = () => setShowPassword(!showPassword);
     const [crossEye, setCrossEye] = useState(false);
     const toggleCrossEye = () => setCrossEye(!crossEye);
+    
+    const [ isPressed, setIsPressed] = useState(false);
+  const handleMouse = { handleMouseDown: () =>{ setIsPressed(true); }, handleMouseUp: () =>{setIsPressed(false); }, handleMouseLeave: () =>{ setIsPressed(false)}
+  }
+
   return (
     <div className="App">
       
@@ -29,12 +34,24 @@ function App() {
         <div className="Input__container">
         <input type={showPassword ? 'text' : 'password'} id="Password" placeholder="Password" required title="This field is required, the password should has Numbers letters and special characters"minLength={6} maxLength={16}/>
         
-        <button id="showpassword" type="button" onClick={togglePasswordVisibility}>{crossEye ? <FiEye onClick={toggleCrossEye}/> : <FiEyeOff onClick={toggleCrossEye}/>}</button><br/>
+        <button id="showpassword" type="button" onClick={() => {
+            togglePasswordVisibility();
+            toggleCrossEye();
+            }}>
+              {crossEye ? <FiEye/> : <FiEyeOff/>}
+              </button><br/>
         </div>
        
 
         <div className="button__container">
-        <button id="login__button" type="submit">Entrar</button>
+
+        <button id="login__button" type="button" 
+        onMouseDown={handleMouse.handleMouseDown}
+         onMouseUp={handleMouse.handleMouseUp} 
+         onMouseLeave={handleMouse.handleMouseLeave} 
+         className={`Button ${isPressed ? 'buttonScaleAnimation' : ''}`}>
+          Entrar
+          </button>
         
         </div>
         </form>
